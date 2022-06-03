fee73ce0b6974a660e07c2e877f2fe4a9bedddac
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-06-03 17:10:51 +0200

    [FEAT]: working on detailed results page

--- frontend/components/ResultComponent.vue
+++ frontend/components/ResultComponent.vue
@@ -1,6 +1,11 @@
 <template>
 	<div>
-		<v-card elevation="2" outlined>
+		<v-card
+			elevation="2"
+			outlined
+			:to="'results/' + result.city"
+			@click="displayResult"
+		>
 			<v-card-text>
 				<h3>{{ result.city }}</h3>
 				<h4>{{ result.country }}</h4>
@@ -16,6 +21,16 @@ export default {
 		result: {
 			type: Object,
 			required: true,
+		},
+	},
+	methods: {
+		displayResult() {
+			this.$router.push({
+				name: '/results/' + this.result.city + '/',
+				params: {
+					result: this.result,
+				},
+			})
 		},
 	},
 }
--- frontend/pages/results/_id.vue
+++ frontend/pages/results/_id.vue
@@ -1,11 +1,19 @@
 <template>
 	<div>
 		<h2>This is a detailed result</h2>
+		<h3>{{ result.city }}</h3>
+		{{ result.country }}
+		{{ result.price }}
 	</div>
 </template>
 <script>
 export default {
 	name: 'detailedPage',
+	data() {
+		return {
+			result: this.$route.params.result,
+		}
+	},
 }
 </script>
 <style>
