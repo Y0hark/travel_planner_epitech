cd367fe2345a2770050e7c035cbb02afd80f2fb6
Alan GARBO <alan-garbo@hotmail.fr>
2022-06-23 15:36:08 +0200

    result component style

--- frontend/components/ResultComponent.vue
+++ frontend/components/ResultComponent.vue
@@ -2,9 +2,11 @@
 	<div>
 		<v-card elevation="2" outlined @click="displayResult">
 			<v-card-text>
-				<h3>{{ result.city }}</h3>
-				<h4>{{ result.country }}</h4>
-				{{ result.price }}
+				<h3 class="Pcolor">{{ result.city }}</h3>
+				<h4 class="Scolor">{{ result.country }}</h4>
+				<v-btn small class="btn">Voir plus
+				 </v-btn>
+				<p class="Price">{{ result.price }}</p>
 			</v-card-text>
 		</v-card>
 	</div>
@@ -29,4 +31,23 @@ export default {
 }
 </script>
 <style>
+
+.Pcolor {
+	color: #00BBAD;
+}
+
+.Scolor {
+	color: #4A8179;
+}
+
+.Price {
+	text-align: right;
+	border-top: 10px;
+}
+
+.btn {
+	float: right;
+	
+}
+
 </style>
